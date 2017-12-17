b2b305137ba7161469b846b96e4ba36d164e07c4
Chris Underhill <cpu22@cam.ac.uk>
2017-12-17 23:19:37 +0000

    Added some basic framework for event handling

--- src/Flocking/main.cpp
+++ src/Flocking/main.cpp
@@ -1,8 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
@@ -11,8 +12,51 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (event.type) {
+			case sf::Event::Closed:
 				window.close();
+				break;
+			case sf::Event::Resized:
+				std::cout << "new width: " << event.size.width << std::endl;
+				std::cout << "new height: " << event.size.height << std::endl;
+				break;
+			case sf::Event::LostFocus:
+				//pause
+				break;
+			case sf::Event::GainedFocus:
+				//resume
+				break;
+			case sf::Event::KeyPressed:
+				switch (event.key.code) {
+				case sf::Keyboard::Escape:
+					window.close();
+					break;
+				case sf::Keyboard::Return:
+					//pause
+					break;
+				}
+				break;
+			case sf::Event::KeyReleased:
+				break;
+			case sf::Event::MouseWheelScrolled:
+				if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
+					float delta = event.mouseWheelScroll.delta;
+					//increase number of flockers
+				}
+				break;
+			case sf::Event::MouseButtonPressed:
+				if (event.mouseButton.button == sf::Mouse::Left) {
+					//add an obstacle maybe
+				} else if (event.mouseButton.button == sf::Mouse::Right) {
+					//remove an obstacle maybe
+				}
+				break;
+			case sf::Event::MouseButtonReleased:
+				break;
+			case sf::Event::MouseMoved:
+				break;
+
+			}
 		}
 
 		window.clear();
